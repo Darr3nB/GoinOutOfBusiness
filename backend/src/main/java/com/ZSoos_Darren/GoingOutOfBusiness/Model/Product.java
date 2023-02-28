2006cb058c1258c0bc6a7c9276615d4e2881b0a4
Z-SooS <zoli.soos98@gmail.com>
2023-02-28 11:52:26 +0100

    Add Product JPA Entity

---
+++ backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/Model/Product.java
@@ -0,0 +1,21 @@
+package com.ZSoos_Darren.GoingOutOfBusiness.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+    @Id
+    Long id;
+    String name;
+    String description;
+    BigDecimal price;
+    ProductType type;
+    Integer inventory;
+}
