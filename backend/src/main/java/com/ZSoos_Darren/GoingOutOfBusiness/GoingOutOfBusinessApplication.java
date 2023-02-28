c01b45d052d7e7831ce56d1f40d767d18f473016
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2023-02-28 10:57:05 +0100

    First commit, empty backend and frontend added.

---
+++ backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/GoingOutOfBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.ZSoos_Darren.GoingOutOfBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoingOutOfBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoingOutOfBusinessApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ZSoos_Darren/GoingOutOfBusiness/GoingOutOfBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZSoos_Darren.GoingOutOfBusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoingOutOfBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
