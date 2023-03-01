12edcfeebb7741d718273e0d4ebc4a0e434b3536
Z-SooS <zoli.soos98@gmail.com>
2023-03-01 09:37:24 +0100

    Add method for searching by ProductType

--- backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/dao/ProductDao.java
+++ backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/dao/ProductDao.java
@@ -1,7 +1,10 @@
 package com.ZSoos_Darren.GoingOutOfBusiness.dao;
 
 import com.ZSoos_Darren.GoingOutOfBusiness.Model.Product;
+import com.ZSoos_Darren.GoingOutOfBusiness.Model.ProductType;
 import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,5 +15,7 @@ public interface ProductDao extends JpaRepository<Product, Long> {
     @Modifying
     @Query("update Product p set p.inventory = p.inventory + :valueChange WHERE p.id = :productId")
     void updateInventory(@Param("productId") Long productId, @Param("valueChange") int valueChange);
+
+    Page<Product> findAllByType(ProductType searchedType, Pageable pageable);
 }
 
