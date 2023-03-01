3317c8e7434b897c040930cd7a72de91659727b6
Z-SooS <zoli.soos98@gmail.com>
2023-03-01 09:42:11 +0100

    Add methods to get product Page objects

---
+++ backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/service/ProductService.java
@@ -0,0 +1,24 @@
+package com.ZSoos_Darren.GoingOutOfBusiness.service;
+
+import com.ZSoos_Darren.GoingOutOfBusiness.Model.Product;
+import com.ZSoos_Darren.GoingOutOfBusiness.Model.ProductType;
+import com.ZSoos_Darren.GoingOutOfBusiness.dao.ProductDao;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ProductService {
+    private ProductDao productDao;
+
+    public Page<Product> getPageOfProductsFromCategory(int page, int numberOfItemsOnPage, ProductType type, String orderBy, String direction) {
+        return productDao.findAllByType(type, PageRequest.of(page, numberOfItemsOnPage,Sort.by(direction,orderBy)));
+    }
+
+    public Page<Product> getPageOfAllProducts(int page, int numberOfItemsOnPage, String orderBy, String direction) {
+        return productDao.findAll(PageRequest.of(page, numberOfItemsOnPage, Sort.by(direction,orderBy)));
+    }
+}
