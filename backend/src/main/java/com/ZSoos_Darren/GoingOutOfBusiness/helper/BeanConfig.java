248f762e671f477b644276d033bc4794a3e27e5c
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2023-03-03 09:13:59 +0100

    FilterChain bean refactored to BeanConfig class

--- backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/helper/BeanConfig.java
+++ backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/helper/BeanConfig.java
@@ -1,5 +1,7 @@
 package com.ZSoos_Darren.GoingOutOfBusiness.helper;
 
+import com.ZSoos_Darren.GoingOutOfBusiness.security.AuthorizationFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,5 +11,13 @@ public class BeanConfig {
     @Bean
     public BCryptPasswordEncoder bCryptFactory() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public FilterRegistrationBean<AuthorizationFilter> authorizationFilterRegistrationBean() {
+        FilterRegistrationBean<AuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new AuthorizationFilter());
+        registrationBean.addUrlPatterns("/api/*"); //TODO Only apply this filter to URLs starting with "/api/", but add more routes
+        return registrationBean;
     }
 }
