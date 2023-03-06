147af4ac1657c088e7e49764ef36cb384e6df806
Z-SooS <zoli.soos98@gmail.com>
2023-03-06 15:17:26 +0100

    Add new tests for ProductService

--- backend/src/test/java/com/ZSoos_Darren/GoingOutOfBusiness/ProductServiceTest.java
+++ backend/src/test/java/com/ZSoos_Darren/GoingOutOfBusiness/ProductServiceTest.java
@@ -31,7 +31,8 @@ public class ProductServiceTest {
 
     private static final int PAGE = 1;
     private static final int PRODUCT_PER_PAGE = 1;
-    private static final String ORDER_COLUMN = "id";
+    private static final String ORDER_COLUMN_ID = "id";
+    private static final String ORDER_COLUMN_INVENTORY = "inventory";
     private static final String ORDER_DIRECTION_DESC_STRING = "desc";
     private static final String ORDER_DIRECTION_ASC_STRING = "asc";
     private static final Sort.Direction ORDER_DIRECTION = Sort.Direction.DESC;
@@ -42,96 +43,52 @@ public class ProductServiceTest {
     @InjectMocks
     private ProductService productService;
 
-    private static final Product testProduct1 = new Product(EXPECTED_ID,EXPECTED_NAME,EXPECTED_DESCRIPTION,EXPECTED_PRICE, EXPECTED_TYPE_OTHER,EXPECTED_INVENTORY);
-    private static final Product testProduct2 = new Product(EXPECTED_ID+1,EXPECTED_NAME,EXPECTED_DESCRIPTION,EXPECTED_PRICE, EXPECTED_TYPE_OTHER,EXPECTED_INVENTORY+1);
-    private static final Product testProduct3 = new Product(EXPECTED_ID+2,EXPECTED_NAME,EXPECTED_DESCRIPTION,EXPECTED_PRICE, EXPECTED_TYPE_OTHER,EXPECTED_INVENTORY+2);
+    private static final Product testProduct1 = new Product(EXPECTED_ID, EXPECTED_NAME, EXPECTED_DESCRIPTION, EXPECTED_PRICE, EXPECTED_TYPE_OTHER, EXPECTED_INVENTORY);
+    private static final Product testProduct2 = new Product(EXPECTED_ID + 1, EXPECTED_NAME, EXPECTED_DESCRIPTION, EXPECTED_PRICE, EXPECTED_TYPE_OTHER, EXPECTED_INVENTORY + 1);
+    private static final Product testProduct3 = new Product(EXPECTED_ID + 2, EXPECTED_NAME, EXPECTED_DESCRIPTION, EXPECTED_PRICE, EXPECTED_TYPE_OTHER, EXPECTED_INVENTORY + 2);
 
 
     @BeforeEach
-    public void setUpEach(){
+    public void setUpEach() {
         MockitoAnnotations.openMocks(this);
     }
 
     @Test
-    public void test_saveProduct_successfulSave_returnsMatchingProduct() {
+    public void saveProduct_successfulSave_returnsMatchingProduct() {
         Mockito.when(mockProductDao.save(testProduct1)).thenReturn(testProduct1);
 
         Product savedProduct = productService.saveProduct(testProduct1);
 
-        Assertions.assertTrue(savedProduct.equals(testProduct1),"Returned product fields should match initial product fields");
+        Assertions.assertEquals(savedProduct, testProduct1, "Returned product fields should match initial product fields");
     }
 
     @Test
-    public void test_getProductForId_productIsInDatabase_returnsCorrectProduct() {
-        Mockito.when(mockProductDao.findById(Mockito.anyLong())).thenReturn(Optional.ofNullable(testProduct1));
+    public void getProductForId_productIsInDatabase_returnsCorrectProduct() {
+        Mockito.when(mockProductDao.findById(Mockito.anyLong())).thenReturn(Optional.of(testProduct1));
 
         Product returnedProduct = productService.getProductForId(EXPECTED_ID);
 
-        Assertions.assertTrue(returnedProduct.equals(testProduct1),"The returned products values should match the expected values");
+        Assertions.assertEquals(returnedProduct, testProduct1, "The returned products values should match the expected values");
     }
 
     @Test
-    public void test_getProductForId_productNotInDatabase_returnsNull() {
+    public void getProductForId_productNotInDatabase_returnsNull() {
         Mockito.when(mockProductDao.findById(Mockito.anyLong())).thenReturn(Optional.empty());
 
         Product returnedProduct = productService.getProductForId(EXPECTED_ID);
 
-        Assertions.assertNull(returnedProduct,"The returned product is should be null");
+        Assertions.assertNull(returnedProduct, "The returned product is should be null");
     }
 
     @Test
-    public void test_getPageOfAllProducts_returnsPageObjectCorrectly() {
-        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct1,testProduct2,testProduct3));
+    public void getPageOfAllProducts_returnsPageObjectCorrectly() {
+        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct1, testProduct2, testProduct3));
         int expectedSize = expectedProducts.size();
-        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE,PRODUCT_PER_PAGE, Sort.by(ORDER_DIRECTION,ORDER_COLUMN)),expectedSize);
+        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE, PRODUCT_PER_PAGE, Sort.by(ORDER_DIRECTION, ORDER_COLUMN_ID)), expectedSize);
 
         Mockito.when(mockProductDao.findAll(Mockito.any(PageRequest.class))).thenReturn(expectedPage);
 
-        Page<Product> returnedPage = productService.getPageOfAllProducts(PAGE,PRODUCT_PER_PAGE,ORDER_COLUMN, ORDER_DIRECTION_DESC_STRING);
-
-        var returnedContent = returnedPage.getContent();
-
-        Assertions.assertEquals(expectedPage.getTotalPages(), returnedPage.getTotalPages(), "Total pages should match expected");
-        Assertions.assertTrue(expectedPage.getContent().size() == returnedPage.getContent().size()
-        && expectedProducts.containsAll(returnedContent) && returnedContent.containsAll(expectedProducts), "Returned content should match expected content");
-    }
-
-    @Test
-    public void test_getPageOfAllProducts_noProductsInDatabase_returnsPageObjectWithEmptyList() {
-        List<Product> emptyList = new ArrayList<>();
-        int expectedSize = 0;
-        Page<Product> expectedPage = new PageImpl<>(emptyList, PageRequest.of(PAGE,PRODUCT_PER_PAGE, Sort.by(ORDER_DIRECTION,ORDER_COLUMN)),expectedSize);
-
-        Mockito.when(mockProductDao.findAll(Mockito.any(PageRequest.class))).thenReturn(expectedPage);
-
-        Page<Product> returnedPage = productService.getPageOfAllProducts(PAGE,PRODUCT_PER_PAGE,ORDER_COLUMN, ORDER_DIRECTION_DESC_STRING);
-
-        Assertions.assertEquals(expectedSize, returnedPage.getContent().size(), "Returned content should be empty");
-    }
-
-    @Test
-    public void test_getPageOfAllProducts_sortIdOrderDesc_returnedListInOrder() {
-        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct1,testProduct2,testProduct3));
-        int expectedSize = expectedProducts.size();
-        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE,PRODUCT_PER_PAGE, Sort.by(Sort.Direction.DESC,ORDER_COLUMN)),expectedSize);
-
-        Mockito.when(mockProductDao.findAll(Mockito.any(PageRequest.class))).thenReturn(expectedPage);
-
-        Page<Product> returnedPage = productService.getPageOfAllProducts(PAGE,PRODUCT_PER_PAGE,ORDER_COLUMN,ORDER_DIRECTION_DESC_STRING);
-        var returnedContent = returnedPage.getContent();
-
-        Assertions.assertTrue(returnedContent.get(0).getId() > returnedContent.get(1).getId() && returnedContent.get(1).getId() > returnedContent.get(2).getId() , "");
-    }
-
-    @Test
-    public void test_getPageOfProductsFromCategory_returnsPageObjectCorrectly() {
-        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct1,testProduct2,testProduct3));
-        int expectedSize = expectedProducts.size();
-        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE,PRODUCT_PER_PAGE, Sort.by(ORDER_DIRECTION,ORDER_COLUMN)),expectedSize);
-
-//        Mockito.when(mockProductDao.findAll(Mockito.any(PageRequest.class))).thenReturn(expectedPage);
-
-        Page<Product> returnedPage = productService.getPageOfProductsFromCategory(PAGE, PRODUCT_PER_PAGE,EXPECTED_TYPE_OTHER,ORDER_COLUMN, ORDER_DIRECTION_DESC_STRING);
+        Page<Product> returnedPage = productService.getPageOfAllProducts(PAGE, PRODUCT_PER_PAGE, ORDER_COLUMN_ID, ORDER_DIRECTION_DESC_STRING);
 
         var returnedContent = returnedPage.getContent();
 
@@ -139,5 +96,118 @@ public class ProductServiceTest {
         Assertions.assertTrue(expectedPage.getContent().size() == returnedPage.getContent().size()
                 && expectedProducts.containsAll(returnedContent) && returnedContent.containsAll(expectedProducts), "Returned content should match expected content");
     }
+
+    @Test
+    public void getPageOfAllProducts_noProductsInDatabase_returnsPageObjectWithEmptyList() {
+        List<Product> emptyList = new ArrayList<>();
+        int expectedSize = 0;
+        Page<Product> expectedPage = new PageImpl<>(emptyList, PageRequest.of(PAGE, PRODUCT_PER_PAGE, Sort.by(ORDER_DIRECTION, ORDER_COLUMN_ID)), expectedSize);
+
+        Mockito.when(mockProductDao.findAll(Mockito.any(PageRequest.class))).thenReturn(expectedPage);
+
+        Page<Product> returnedPage = productService.getPageOfAllProducts(PAGE, PRODUCT_PER_PAGE, ORDER_COLUMN_ID, ORDER_DIRECTION_DESC_STRING);
+
+        Assertions.assertEquals(expectedSize, returnedPage.getContent().size(), "Returned content should be empty");
+    }
+
+    @Test
+    public void getPageOfAllProducts_sortIdOrderDesc_returnedListInOrder() {
+        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct3, testProduct2, testProduct1));
+        int expectedSize = expectedProducts.size();
+        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE, PRODUCT_PER_PAGE, Sort.by(Sort.Direction.DESC, ORDER_COLUMN_ID)), expectedSize);
+
+        Mockito.when(mockProductDao.findAll(Mockito.any(PageRequest.class))).thenReturn(expectedPage);
+
+        Page<Product> returnedPage = productService.getPageOfAllProducts(PAGE, PRODUCT_PER_PAGE, ORDER_COLUMN_ID, ORDER_DIRECTION_DESC_STRING);
+        var returnedContent = returnedPage.getContent();
+
+        Assertions.assertEquals(expectedSize, returnedContent.size(), "Content list size should be same as expected");
+        Assertions.assertTrue(returnedContent.get(0).getId() > returnedContent.get(1).getId() &&
+                        returnedContent.get(1).getId() > returnedContent.get(2).getId(),
+                "Content list is sorted by 'id' column and descending order");
+    }
+
+    @Test
+    public void getPageOfAllProducts_sortInventoryOrderAsc_returnedListInOrder() {
+        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct1, testProduct2, testProduct3));
+        int expectedSize = expectedProducts.size();
+        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE, PRODUCT_PER_PAGE, Sort.by(Sort.Direction.ASC, ORDER_COLUMN_INVENTORY)), expectedSize);
+
+        Mockito.when(mockProductDao.findAll(Mockito.any(PageRequest.class))).thenReturn(expectedPage);
+
+        Page<Product> returnedPage = productService.getPageOfAllProducts(PAGE, PRODUCT_PER_PAGE, ORDER_COLUMN_ID, ORDER_DIRECTION_ASC_STRING);
+        var returnedContent = returnedPage.getContent();
+
+        Assertions.assertEquals(expectedSize, returnedContent.size(), "Content list size should be same as expected");
+        Assertions.assertTrue(returnedContent.get(2).getInventory() > returnedContent.get(1).getInventory() &&
+                        returnedContent.get(1).getInventory() > returnedContent.get(0).getInventory(),
+                "Content list is sorted by 'inventory' column and ascending order");
+    }
+
+    @Test
+    public void getPageOfProductsFromCategory_searchCategoryOther_returnsPageObjectCorrectly() {
+        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct1, testProduct2, testProduct3));
+        int expectedSize = expectedProducts.size();
+        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE, PRODUCT_PER_PAGE, Sort.by(ORDER_DIRECTION, ORDER_COLUMN_ID)), expectedSize);
+
+        Mockito.when(mockProductDao.findAllByType(Mockito.any(ProductType.class), Mockito.any(PageRequest.class))).thenReturn(expectedPage);
+
+        Page<Product> returnedPage = productService.getPageOfProductsFromCategory(PAGE, PRODUCT_PER_PAGE, EXPECTED_TYPE_OTHER, ORDER_COLUMN_ID, ORDER_DIRECTION_DESC_STRING);
+
+        var returnedContent = returnedPage.getContent();
+
+        Assertions.assertEquals(expectedPage.getTotalPages(), returnedPage.getTotalPages(), "Total pages should match expected");
+        Assertions.assertTrue(returnedContent.stream().allMatch(p -> p.getType() == EXPECTED_TYPE_OTHER), "Every returned object has expected type");
+        Assertions.assertTrue(expectedPage.getContent().size() == returnedPage.getContent().size()
+                && expectedProducts.containsAll(returnedContent) && returnedContent.containsAll(expectedProducts), "Returned content should match expected content");
+    }
+
+    @Test
+    public void getPageOfProductsFromCategory_noProductsInDatabase_returnsPageObjectWithEmptyList() {
+        List<Product> emptyList = new ArrayList<>();
+        int expectedSize = 0;
+        Page<Product> expectedPage = new PageImpl<>(emptyList, PageRequest.of(PAGE, PRODUCT_PER_PAGE, Sort.by(ORDER_DIRECTION, ORDER_COLUMN_ID)), expectedSize);
+
+        Mockito.when(mockProductDao.findAllByType(Mockito.any(ProductType.class), Mockito.any(PageRequest.class))).thenReturn(expectedPage);
+
+        Page<Product> returnedPage = productService.getPageOfProductsFromCategory(PAGE, PRODUCT_PER_PAGE, EXPECTED_TYPE_OTHER, ORDER_COLUMN_ID, ORDER_DIRECTION_DESC_STRING);
+
+        Assertions.assertEquals(expectedSize, returnedPage.getContent().size(), "Returned content should be empty");
+    }
+
+    @Test
+    public void getPageOfProductsFromCategory_sortIdOrderDesc_returnedListInOrder() {
+        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct3, testProduct2, testProduct1));
+        int expectedSize = expectedProducts.size();
+        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE, PRODUCT_PER_PAGE, Sort.by(Sort.Direction.DESC, ORDER_COLUMN_ID)), expectedSize);
+
+        Mockito.when(mockProductDao.findAllByType(Mockito.any(ProductType.class), Mockito.any(PageRequest.class))).thenReturn(expectedPage);
+
+        Page<Product> returnedPage = productService.getPageOfProductsFromCategory(PAGE, PRODUCT_PER_PAGE, EXPECTED_TYPE_OTHER, ORDER_COLUMN_ID, ORDER_DIRECTION_DESC_STRING);
+        var returnedContent = returnedPage.getContent();
+
+        Assertions.assertEquals(expectedSize, returnedContent.size(), "Content list size should be same as expected");
+        Assertions.assertTrue(returnedContent.get(0).getId() > returnedContent.get(1).getId() &&
+                        returnedContent.get(1).getId() > returnedContent.get(2).getId(),
+                "Content list is sorted by 'id' column and descending order");
+    }
+
+    @Test
+    public void getPageOfProductsFromCategory_sortInventoryOrderAsc_returnedListInOrder() {
+        List<Product> expectedProducts = new ArrayList<>(List.of(testProduct1, testProduct2, testProduct3));
+        int expectedSize = expectedProducts.size();
+        Page<Product> expectedPage = new PageImpl<>(expectedProducts, PageRequest.of(PAGE, PRODUCT_PER_PAGE, Sort.by(Sort.Direction.ASC, ORDER_COLUMN_INVENTORY)), expectedSize);
+
+        Mockito.when(mockProductDao.findAllByType(Mockito.any(ProductType.class), Mockito.any(PageRequest.class))).thenReturn(expectedPage);
+
+        Page<Product> returnedPage = productService.getPageOfProductsFromCategory(PAGE, PRODUCT_PER_PAGE, EXPECTED_TYPE_OTHER, ORDER_COLUMN_ID, ORDER_DIRECTION_ASC_STRING);
+        var returnedContent = returnedPage.getContent();
+
+        Assertions.assertEquals(expectedSize, returnedContent.size(), "Content list size should be same as expected");
+        Assertions.assertTrue(returnedContent.get(2).getInventory() > returnedContent.get(1).getInventory() &&
+                        returnedContent.get(1).getInventory() > returnedContent.get(0).getInventory(),
+                "Content list is sorted by 'inventory' column and ascending order");
+    }
+
 
 }
