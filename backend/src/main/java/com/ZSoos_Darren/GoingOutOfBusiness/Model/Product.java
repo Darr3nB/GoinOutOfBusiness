e9d914cbc278168bb74fc1b4cafd7a224b29b25c
Z-SooS <zoli.soos98@gmail.com>
2023-03-06 10:38:35 +0100

    Add equals method and AllArgsConstructor annotation

--- backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/Model/Product.java
+++ backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/Model/Product.java
@@ -7,11 +7,13 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     @Id
     @GeneratedValue(generator = "increment-generator")
@@ -25,4 +27,24 @@ public class Product {
     @Enumerated(EnumType.STRING)
     ProductType type;
     Integer inventory;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        if (o.getClass() != this.getClass()) {
+            return false;
+        }
+
+        final Product other = (Product) o;
+
+        return (Objects.equals(this.id, other.id) &&
+                Objects.equals(this.name, other.name) &&
+                Objects.equals(this.description,other.description) &&
+                Objects.equals(this.price,other.price) &&
+                Objects.equals(this.type,other.type) &&
+                Objects.equals(this.inventory,other.inventory));
+    }
 }
