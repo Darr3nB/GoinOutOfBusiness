5de331ab257d705bf66c13e2c7c085edc814ec3e
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2023-03-07 11:01:18 +0100

    Utility js file added with api request and email validation

--- frontend/src/components/LoginModal.jsx
+++ frontend/src/components/LoginModal.jsx
@@ -1,6 +1,36 @@
+import {utility} from "../utility.js";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
+
 export default function LoginModal({open, closeModal}) {
     if (!open) {
         return null
+    }
+
+    const navigate = useNavigate();
+    const [loginBtn, setLoginBtn] = useState(false);
+    const [loginPtag, setLoginPtag] = useState(false);
+
+
+    const performLogin = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+
+        utility.apiPostWithDictionary(`/user/login`,
+            {'email': formData.get('username-field'), 'password': formData.get('password-field')})
+            .then(response => {
+                if (response.ok) {
+                    navigate(0);
+                }
+            })
+    }
+
+    const checkEmail = (email) => {
+        if (!utility.validateEmail(email)) {
+            setLoginPtag(true);
+        } else {
+            setLoginPtag(false);
+        }
     }
 
     return (
@@ -8,10 +38,14 @@ export default function LoginModal({open, closeModal}) {
             <div className="modal-container">
                 <div>Login to GOOB</div>
                 <p className="closeOnModal" onClick={closeModal}>X</p>
-                <br/><input type="text" placeholder="Username"/>
-                <input type="text" placeholder="Password"/>
-                <br/>
-                <button type="button">Login</button>
+                <form onSubmit={event => performLogin(event)}>
+                    <br/><input onInput={(event) => checkEmail(event.target.value)} type="text" placeholder="Email"
+                                name="email-field"/>
+                    <p className={loginPtag ? 'visible-login-p' : 'invisible-login-p'}>{loginPtag ? 'Invalid e-mail format' : ''}</p>
+                    <input type="text" placeholder="Password" name="password-field"/>
+                    <br/>
+                    <button>Login</button>
+                </form>
             </div>
         </div>
     );
