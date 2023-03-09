5782744a5a52a3c0f5515e957176247d6efea5e3
Z-SooS <zoli.soos98@gmail.com>
2023-03-09 14:25:24 +0100

    Fix filter paging with references

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -10,6 +10,14 @@ export default function HomePage() {
     const categories = useRef([]);
     const [pageTurned, setPageTurned] = useState(true);
 
+    const [filtered, setFiltered] = useState(false);
+
+    const categoryRef = useRef();
+    const searchNameRef = useRef();
+    const fromRef = useRef();
+    const toRef = useRef();
+    const directionRef = useRef();
+
     const getCategories = async () => {
         const response = await utility.apiGet(`/products/get-categories`);
         categories.current = await response.json();
@@ -23,40 +31,51 @@ export default function HomePage() {
             setAllProducts(data.content);
         }
 
-        fetchData();
         getCategories();
-    }, [pageTurned]);
+        if (filtered) {
+            sendfilterRequest();
+            return;
+        }
+        fetchData();
+    }, [currentPageCount]);
 
     const turnPage = (pageNumber) => {
         if (pageNumber === null || currentPageCount === pageNumber || pageNumber <= 0 || pageNumber > totalPageCount.current) {
             return;
         }
         setCurrentPageCount(pageNumber);
-        setPageTurned(!pageTurned);
     }
 
     const filterItems = (event) => {
         event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const category = formData.get('category') === "null" ? null : formData.get('category');
-        const name = formData.get('name-search-input').length <= 0 ? null : formData.get('name-search-input');
-        const from = formData.get('min-price-search-input') <= 0 ? 0 : formData.get('min-price-search-input');
-        const to = formData.get('max-price-search-input') <= 1 ? Number.MAX_SAFE_INTEGER : formData.get('max-price-search-input');
-        
+
+        setFiltered(true);
+        if (currentPageCount === 1) {
+            sendfilterRequest();
+        }
+        setCurrentPageCount(1)
+    }
+
+    const sendfilterRequest = async () => {
+        const category = categoryRef.current.value === "null" ? null : categoryRef.current.value;
+        const name = searchNameRef.current.value.length <= 0 ? null : searchNameRef.current.value;
+        const from = fromRef.current.value.length <= 0 ? 0 : fromRef.current.value;
+        const to = toRef.current.value.length <= 1 ? Number.MAX_SAFE_INTEGER : toRef.current.value;
+        const direction = directionRef.current.value;
+
         if (from >= to) {
             alert("Price range 'to' cannot be higher than 'from'!");
             return;
         }
 
-        utility.apiGet(`/products/search/0?name=${name === null ? '' : name}&from=${from}&to=${to}&direction=${formData.get('direction')}${category === null ? '' : `&category=${category}`}`)
+        const data = await utility.apiGet(`/products/search/${currentPageCount - 1}?name=${name === null ? '' : name}&from=${from}&to=${to}&direction=${direction}${category === null ? '' : `&category=${category}`}`)
             .then(response => {
                 if (response.ok) {
-                    response.json().then(data => {
-                        setAllProducts(data.content);
-                        setCurrentPageCount(1);
-                    })
+                    return response.json();
                 }
             });
+        setAllProducts(data.content);
+        totalPageCount.current = data.totalPages;
     }
 
 
@@ -75,14 +94,15 @@ export default function HomePage() {
                             Filter
                             <form onSubmit={event => filterItems(event)}>
                                 <label htmlFor="name-search-input">Name:</label>
-                                <br/><input type="text" name="name-search-input" minLength="3"/>
+                                <br/><input type="text" name="name-search-input" minLength="3" ref={searchNameRef}/>
 
                                 {/*TODO get all types from backend, list proper options based on that*/}
                                 <label htmlFor="category">Category: </label><br/>
-                                <select name="category" id="category">
+                                <select name="category" id="category" ref={categoryRef}>
                                     <option value="null"></option>
                                     {categories.current?.map((cat, index) => {
-                                        return <option key={`cat-key${index}`} value={cat}>{cat.charAt(0).toUpperCase() + cat.slice(1).toLowerCase()}</option>
+                                        return <option key={`cat-key${index}`}
+                                                       value={cat}>{cat.charAt(0).toUpperCase() + cat.slice(1).toLowerCase()}</option>
                                     })}
                                 </select>
 
@@ -90,15 +110,16 @@ export default function HomePage() {
                                 <p>Price range:</p>
                                 <label htmlFor="min-price-search-input">From: </label>
                                 <input type="number" name="min-price-search-input"
-                                       className="price-search-field"/>
+                                       className="price-search-field" ref={fromRef}/>
                                 <br/><label htmlFor="max-price-search-input">To: </label>
-                                <input name="max-price-search-input" type="number" className="price-search-field"/>
+                                <input name="max-price-search-input" type="number" className="price-search-field"
+                                       ref={toRef}/>
                                 <br/>
                                 <label htmlFor="direction">Direction: </label>
-                                <br/><select name="order-by" id="direction">
-                                    <option value="desc">Descending</option>
-                                    <option value="asc">Ascending</option>
-                                </select>
+                                <br/><select name="order-by" id="direction" ref={directionRef}>
+                                <option value="desc">Descending</option>
+                                <option value="asc">Ascending</option>
+                            </select>
                                 <br/>
                                 <button>Search</button>
                             </form>
