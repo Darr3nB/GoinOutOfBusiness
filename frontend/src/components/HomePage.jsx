6d842a7542e1d847b4512ba3cdd2b8329b1a5b54
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2023-03-13 11:00:33 +0100

    Repositioned searc-bar div

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -89,50 +89,50 @@ export default function HomePage() {
                     <Link to={`https://www.codecool.com`} target="_blank"><img src="../src/assets/hp_meme.jpg"
                                                                                alt="meme" className="meme"/></Link>
                 </div>
-                <div className="main-content">
-                    <div className="search-container">
-                        <div className="search-bar">
-                            Filter
-                            <form onSubmit={event => filterItems(event)}>
-                                <label htmlFor="name-search-input">Name:</label>
-                                <br/><input type="text" name="name-search-input" minLength="3" ref={searchNameRef}/>
+                <div className="search-container">
+                    <div className="search-bar">
+                        Filter
+                        <form onSubmit={event => filterItems(event)}>
+                            <label htmlFor="name-search-input">Name:</label>
+                            <br/><input type="text" name="name-search-input" minLength="3" ref={searchNameRef}/>
 
-                                {/*TODO get all types from backend, list proper options based on that*/}
-                                <label htmlFor="category">Category: </label><br/>
-                                <select name="category" id="category" ref={categoryRef}>
-                                    <option value="null"></option>
-                                    {categories.current?.map((cat, index) => {
-                                        return <option key={`cat-key${index}`}
-                                                       value={cat}>{cat.charAt(0).toUpperCase() + cat.slice(1).toLowerCase()}</option>
-                                    })}
-                                </select>
-
-                                {/*TODO make it as a slide*/}
-                                <p>Price range:</p>
-                                <label htmlFor="min-price-search-input">From: </label>
-                                <input type="number" name="min-price-search-input"
-                                       className="price-search-field" ref={fromRef}/>
-                                <br/><label htmlFor="max-price-search-input">To: </label>
-                                <input name="max-price-search-input" type="number" className="price-search-field"
-                                       ref={toRef}/>
-                                <br/>
-                                <label htmlFor={"order-by"}>Order By:</label>
-                                <select name={"order-by"} ref={orderColumnRef}>
-                                    <option value={"name"}>Name</option>
-                                    <option value={"price"}>Price</option>
-                                    <option value={"type"}>Category</option>
-                                    <option value={"inventory"}>Inventory</option>
-                                </select>
-                                <label htmlFor="direction">Direction: </label>
-                                <br/><select name="direction" id="direction" ref={directionRef}>
-                                <option value="desc">Descending</option>
-                                <option value="asc">Ascending</option>
+                            {/*TODO get all types from backend, list proper options based on that*/}
+                            <br/><label htmlFor="category">Category: </label><br/>
+                            <select name="category" id="category" ref={categoryRef}>
+                                <option value="null"></option>
+                                {categories.current?.map((cat, index) => {
+                                    return <option key={`cat-key${index}`}
+                                                   value={cat}>{cat.charAt(0).toUpperCase() + cat.slice(1).toLowerCase()}</option>
+                                })}
                             </select>
-                                <br/>
-                                <button>Search</button>
-                            </form>
-                        </div>
+
+                            {/*TODO make it as a slide*/}
+                            <p>Price range:</p>
+                            <label htmlFor="min-price-search-input">From: </label>
+                            <input type="number" name="min-price-search-input"
+                                   className="price-search-field" ref={fromRef}/>
+                            <br/><label htmlFor="max-price-search-input">To: </label>
+                            <input name="max-price-search-input" type="number" className="price-search-field"
+                                   ref={toRef}/>
+                            <br/>
+                            <label htmlFor={"order-by"}>Order By:</label>
+                            <select name={"order-by"} ref={orderColumnRef}>
+                                <option value={"name"}>Name</option>
+                                <option value={"price"}>Price</option>
+                                <option value={"type"}>Category</option>
+                                <option value={"inventory"}>Inventory</option>
+                            </select>
+                            <br/><label htmlFor="direction">Direction: </label>
+                            <br/><select name="direction" id="direction" ref={directionRef}>
+                            <option value="desc">Descending</option>
+                            <option value="asc">Ascending</option>
+                        </select>
+                            <br/>
+                            <button>Search</button>
+                        </form>
                     </div>
+                </div>
+                <div className="main-content">
                     <div className="main-page-div">
                         {allProducts.map(data => {
                             return <div className="card-key-div" key={"key-" + data.id}><ProductCard product={data}/>
