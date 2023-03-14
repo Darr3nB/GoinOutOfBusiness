e0f24695bef77553508793265b09c8ff0f84ee47
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2023-03-14 11:45:42 +0100

    Total 14 picture added to the db and sql

--- frontend/src/routes/Registration.jsx
+++ frontend/src/routes/Registration.jsx
@@ -16,25 +16,26 @@ export default function Registration() {
 
     const performRegistration = async (event) => {
         event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const profilePicture = uploadedImage === null ? utility.questionMarkPicture : uploadedImage;
-
-        if (!passwordMatcher(formData.get("password-field"), formData.get("password-again-field"))) {
-            alert("Passwords must match!");
-            return;
-        }
-
-        await utility.apiPostWithDictionary(`/user/registration`, {
-            'email': formData.get("email-field"),
-            'password': formData.get("password-field"),
-            'passwordAgain': formData.get("password-again-field"),
-            'dateOfBirth': null,
-            'profilePicture': profilePicture,
-        }).then(response => {
-            if (response.ok) {
-                navigate(0);
-            }
-        })
+        console.log(uploadedImage);
+        // const formData = new FormData(event.currentTarget);
+        // const profilePicture = uploadedImage === null ? utility.questionMarkPicture : uploadedImage;
+        //
+        // if (!passwordMatcher(formData.get("password-field"), formData.get("password-again-field"))) {
+        //     alert("Passwords must match!");
+        //     return;
+        // }
+        //
+        // await utility.apiPostWithDictionary(`/user/registration`, {
+        //     'email': formData.get("email-field"),
+        //     'password': formData.get("password-field"),
+        //     'passwordAgain': formData.get("password-again-field"),
+        //     'dateOfBirth': null,
+        //     'profilePicture': profilePicture,
+        // }).then(response => {
+        //     if (response.ok) {
+        //         navigate(0);
+        //     }
+        // })
     }
 
     const uploadImage = async (event) => {
