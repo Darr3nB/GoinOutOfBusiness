f63e96e4b6ec4bce1ebac15793c9ef5360eaf7c6
Z-SooS <zoli.soos98@gmail.com>
2023-03-14 14:27:03 +0100

    Created headers for use in handler class

---
+++ GoobImage/headers/handler.hpp
@@ -0,0 +1,36 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+#include <iostream>
+#include "stdafx.hpp"
+#include "filesystem.hpp"
+
+
+using namespace std;
+using namespace web;
+using namespace http;
+using namespace utility;
+using namespace http::experimental::listener;
+
+class handler
+{
+public:
+    handler();
+    handler(utility::string_t url, filesystem::path product_image_path);
+    virtual ~handler();
+
+    pplx::task<void>open() { return m_listener.open(); }
+    pplx::task<void>close() { return m_listener.close(); }
+
+protected:
+
+private:
+    void handle_get(http_request message);
+    void handle_put(http_request message);
+    void handle_post(http_request message);
+    void handle_delete(http_request message);
+    void handle_error(pplx::task<void>& t);
+    http_listener m_listener;
+    filesystem::path product_dir_path_;
+};
+
+#endif // HANDLER_H
