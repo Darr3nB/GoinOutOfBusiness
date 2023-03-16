372f29ca0aaf57f297689b703d9a2ee8862af5e2
DESKTOP-25963A0\Dávid <david.nemeth24@gmail.com>
2023-03-16 09:48:37 +0100

    Sending e-mail on registration implemented.

--- backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/helper/BeanConfig.java
+++ backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/helper/BeanConfig.java
@@ -4,7 +4,11 @@ import com.ZSoos_Darren.GoingOutOfBusiness.security.AuthorizationFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Properties;
 
 @Configuration
 public class BeanConfig {
@@ -19,5 +23,23 @@ public class BeanConfig {
         registrationBean.setFilter(new AuthorizationFilter());
         registrationBean.addUrlPatterns("/api/*"); //TODO Only apply this filter to URLs starting with "/api/", but add more routes
         return registrationBean;
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername(System.getenv("EMAIL_ADDRESS"));
+        mailSender.setPassword(System.getenv("EMAIL_PASSWORD"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
     }
 }
