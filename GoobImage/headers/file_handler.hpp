af504327ab590acbb3e93dadffb197d0a90fd145
Z-SooS <zoli.soos98@gmail.com>
2023-03-23 12:06:19 +0100

    Implement save_files static method in file_handler

--- GoobImage/headers/file_handler.hpp
+++ GoobImage/headers/file_handler.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <codecvt>
 #include <string_view>
 
 #include "stdafx.hpp"
@@ -21,9 +22,68 @@ public:
 
 		return streams::fstream::open_istream(file_path, std::ios::in).get();
 	}
-	static string_t save_file()
+	static std::vector<std::string> save_files(concurrency::streams::istream message_body)
 	{
-		return U("a");
+		struct m_file
+		{
+			std::string file_name;
+			std::string raw_data;
+		};
+
+		container_buffer<std::string> buffer;
+		message_body.read_to_end(buffer).get();
+		std::string contents = buffer.collection();
+		std::stringstream ss(contents);
+		
+		std::vector<std::string> linesInStream;
+		std::string line;
+		while (std::getline(ss, line)) {
+			linesInStream.push_back(line);
+		}
+
+		int file_count = 0;
+		std::vector<m_file> files;
+		int data_begin_index;
+		int data_end_index = 0;
+		for (int i = 2; i < linesInStream.size(); i++)
+		{
+			if (linesInStream[i].find("image/jpeg") != string::npos)
+			{
+				file_count++;
+				files.push_back(m_file());
+				last_file_name++;
+				files.at(file_count - 1).file_name = std::to_string(last_file_name) + ".jpg";
+				data_begin_index = i + 2;
+			}
+			else if (linesInStream[i].find("image/png") != string::npos)
+			{
+				file_count++;
+				files.push_back(m_file());
+				last_file_name++;
+				files.at(file_count - 1).file_name = std::to_string(last_file_name) + ".png";
+				data_begin_index = i + 2;
+			}
+			else if (linesInStream[i].starts_with("----"))
+			{
+				data_end_index = i - 1;
+			}
+			if (data_end_index > data_begin_index)
+			{
+				for (int j = data_begin_index; j < data_end_index; j++)
+				{
+					files.at(file_count - 1).raw_data.append(linesInStream[j] + '\n');
+				}
+				static std::wstring_convert<codecvt_utf8_utf16<wchar_t>> converter;
+				std::ofstream out_file(converter.to_bytes(product_image_path) + "/" + files.at(file_count - 1).file_name, std::ios::binary);
+				out_file << files.at(file_count - 1).raw_data;
+				out_file.close();
+			}
+
+		}
+
+		vector<string> file_names;
+		for (const auto& file : files) file_names.push_back(file.file_name);
+		return file_names;
 	}
 	static int get_last_product_filename()
 	{
