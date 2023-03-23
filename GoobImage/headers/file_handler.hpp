56537e816f8455a389b7830f11586c213a1811cd
Z-SooS <zoli.soos98@gmail.com>
2023-03-23 11:04:50 +0100

    Add function to read last saved products name

--- GoobImage/headers/file_handler.hpp
+++ GoobImage/headers/file_handler.hpp
@@ -25,7 +25,42 @@ public:
 	{
 		return U("a");
 	}
+	static int get_last_product_filename()
+	{
+		int max = 0;
+		for (const auto& entry : filesystem::directory_iterator(product_image_path))
+		{
+			int file_number = convert_to_pos_int(entry.path().filename().stem().generic_string().c_str());
+			if (file_number > max) max = file_number;
+		}
+
+		return max;
+	}
 private:
 	/*static constexpr std::string product_image_path = "product";*/
 	static inline string_t product_image_path = U("product");
+	static inline int last_file_name = get_last_product_filename();
+	static int convert_to_pos_int(char const* s)
+	{
+		if (s == NULL || *s == '\0')
+			return -1;
+
+		bool negate = (s[0] == '-');
+		if (*s == '+' || *s == '-')
+			++s;
+
+		if (*s == '\0')
+			return -1;
+
+		int result = 0;
+		while (*s)
+		{
+			if (*s < '0' || *s > '9')
+				return -1;
+			result = result * 10 + (*s - '0');
+			++s;
+		}
+		return result;
+	}
 };
+
