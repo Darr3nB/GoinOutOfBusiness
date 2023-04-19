9e8321cc659a34df49dc409bd696bdf8a9030aa4
Z-SooS <zoli.soos98@gmail.com>
2023-04-19 11:04:20 +0200

    Add fromString static method to Role enum

--- backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/model/Role.java
+++ backend/src/main/java/com/ZSoos_Darren/GoingOutOfBusiness/model/Role.java
@@ -2,5 +2,10 @@ package com.ZSoos_Darren.GoingOutOfBusiness.model;
 
 public enum Role {
     ADMIN,
-    USER
+    USER;
+
+    public static Role fromString(String roleString) {
+        if (roleString == null) return null;
+        return roleString.toUpperCase().equals(ADMIN.toString()) ? ADMIN : USER;
+    }
 }
